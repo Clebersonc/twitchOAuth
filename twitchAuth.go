0e4d27409846705f189fa02f66d5e5cd8bedcb88
SimplySerenity <mcgoughboy@gmail.com>
2017-08-12 11:28:37 -0700

    Added basic error handling, ability to specify scopes, and dumb token storage

--- twitchAuth.go
+++ twitchAuth.go
@@ -7,6 +7,10 @@ import (
 	"github.com/skratchdot/open-golang/open"
 	"time"
 	"context"
+	"strings"
+	"errors"
+	"os"
+	"io/ioutil"
 )
 
 func tokenReceived(tokenChannel chan string) func(http.ResponseWriter, *http.Request){
@@ -17,20 +21,35 @@ func tokenReceived(tokenChannel chan string) func(http.ResponseWriter, *http.Req
 			w.WriteHeader(http.StatusOK)
 			tokenChannel <- token
 		} else {
+			tokenChannel <- "failed"
 			w.WriteHeader(http.StatusFailedDependency)
 		}
 	}
 }
 
-func GetToken(clientid string)(token string){
+func GetToken(clientid string, scopes []string)(token string, err error){
 	importPath := "github.com/simplyserenity/twitchOAuth"
 
 	p, err := build.Default.Import(importPath, "", build.FindOnly)
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 
 	fs := http.FileServer(http.Dir(filepath.Join(p.Dir, "static")))
+
+	confFile, oErr := os.OpenFile(p.Dir + "/config.dat", os.O_RDWR|os.O_CREATE, os.ModePerm)
+
+	if oErr != nil {
+		return "", oErr
+	}
+
+	content, fErr := ioutil.ReadFile(p.Dir + "/config.dat")
+
+	if fErr != nil {
+		return "", fErr
+	} else if string(content) != "" {
+		return string(content), nil
+	}
 
 	tokenChannel := make(chan string)
 	handleToken := tokenReceived(tokenChannel)
@@ -45,14 +64,26 @@ func GetToken(clientid string)(token string){
 	}()
 
 	//log.Println("Server started!")
-
-	open.Run("https://api.twitch.tv/kraken/oauth2/authorize?client_id=" + clientid + "&redirect_uri=http://localhost:8080/authorize.html&response_type=token&scope=chat_login+user_read")
+	formattedScopes := strings.Join(scopes, "+")
+	open.Run("https://api.twitch.tv/kraken/oauth2/authorize?client_id=" + clientid + "&redirect_uri=http://localhost:8080/authorize.html&response_type=token&scope="+formattedScopes)
 
 	uToken := <- tokenChannel
+
+	if uToken == "failed" {
+		return "", errors.New("The user was redirected, but with no token. Maybe the queried manually for some reason?")
+	}
 
 	ctx, _ := context.WithTimeout(context.Background(), 1 * time.Second)
 
 	srv.Shutdown(ctx)
 
-	return uToken
+	confFile.WriteString(uToken)
+
+	confFile.Sync()
+
+	if cErr := confFile.Close(); cErr != nil {
+		return "", cErr
+	}
+
+	return uToken, nil
 }
